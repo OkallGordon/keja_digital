a05f2087e111d58d8d77fb268c267ebfb4d96f53
G-ordon <gordonochieng5454@gmail.com>
2024-12-19 15:38:29 +0300

    created a real time notification using pub sub for pop up on admins side

--- lib/keja_digital/agreements/tenant_agreement_live.ex
+++ lib/keja_digital/agreements/tenant_agreement_live.ex
@@ -13,6 +13,7 @@ defmodule KejaDigital.Agreements.TenantAgreementLive do
     field :start_date, :date
     field :agreement_content, :string
     field :signed_at, :naive_datetime
+    field :submitted, :boolean, default: false
 
     field :status, :string, default: "pending_review"
 
@@ -21,13 +22,14 @@ defmodule KejaDigital.Agreements.TenantAgreementLive do
 
   def changeset(tenant_agreement_live, attrs) do
     tenant_agreement_live
-    |> cast(attrs, [:tenant_name, :tenant_address, :tenant_phone, :rent, :late_fee, :deposit, :signature, :start_date, :agreement_content, :status])
-    |> validate_required([:tenant_name, :tenant_address, :tenant_phone, :rent, :deposit, :signature, :start_date, :status])
+    |> cast(attrs, [:tenant_name, :tenant_address, :tenant_phone, :rent, :late_fee, :deposit, :signature, :start_date, :agreement_content, :status, :submitted])
+    |> validate_required([:tenant_name, :tenant_address, :tenant_phone, :rent, :deposit, :signature, :start_date, :status, :submitted])
     |> validate_length(:tenant_name, min: 2, max: 100)
     |> validate_length(:tenant_phone, min: 10, max: 15)
     |> validate_format(:tenant_phone, ~r/^[0-9+]+$/, message: "must contain only numbers and plus sign")
     |> validate_number(:rent, greater_than: 0)
     |> validate_number(:deposit, greater_than: 0)
-    |> validate_inclusion(:status, ["pending_review", "approved", "rejected"])  # This validation is fine for string fields
+    |> validate_inclusion(:status, ["pending_review", "approved", "rejected"])
+    |> unique_constraint(:tenant_name, message: "has already submitted an agreement")
   end
 end

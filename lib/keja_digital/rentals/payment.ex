707494fd1fc4ad4109223fefd793dedf6925ba31
G-ordon <gordonochieng5454@gmail.com>
2024-12-25 18:37:27 +0300

    replaced tenant_is to user_id

--- lib/keja_digital/rentals/payment.ex
+++ lib/keja_digital/rentals/payment.ex
@@ -7,6 +7,8 @@ defmodule KejaDigital.Rentals.Payment do
     field :due_date, :date
     field :status, :string, default: "pending"
     field :days_overdue, :integer, virtual: true
+    field :door_number, :string
+
     belongs_to :users, KejaDigital.Store.User
     belongs_to :unit, KejaDigital.Store.DoorNumber
 
@@ -15,10 +17,10 @@ defmodule KejaDigital.Rentals.Payment do
 
   def changeset(payment, attrs) do
     payment
-    |> cast(attrs, [:amount, :due_date, :status, :tenant_id, :unit_id])
-    |> validate_required([:amount, :due_date, :status, :tenant_id, :unit_id])
+    |> cast(attrs, [:amount, :due_date, :status, :user_id, :door_number])
+    |> validate_required([:amount, :due_date, :status, :user_id, :door_number])
     |> validate_number(:amount, greater_than: 0)
-    |> assoc_constraint(:tenant)
+    |> assoc_constraint(:user_id)
     |> assoc_constraint(:unit)
   end
 end

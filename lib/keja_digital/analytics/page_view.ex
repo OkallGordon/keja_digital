1fa34c60a24dc27960c64c8889ae6c8af7d0cea0
G-ordon <gordonochieng5454@gmail.com>
2025-01-20 16:14:58 +0300

    create a folder for the analysis

---
+++ lib/keja_digital/analytics/page_view.ex
@@ -0,0 +1,21 @@
+defmodule KejaDigital.Analytics.PageView do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "page_views" do
+    field :path, :string
+    field :view_count, :integer, default: 0
+    field :viewed_on, :date
+    field :ip_address, :string
+    field :user_agent, :string
+    field :referrer, :string
+
+    timestamps()
+  end
+
+  def changeset(page_view, attrs) do
+    page_view
+    |> cast(attrs, [:path, :view_count, :viewed_on, :ip_address, :user_agent, :referrer])
+    |> validate_required([:path, :view_count, :viewed_on])
+  end
+end

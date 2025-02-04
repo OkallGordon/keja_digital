a3b376d1eb72668dfef5356feed2f7c34e4226e3
OkallGordon <gordonochieng5454@gmail.com>
2025-02-04 18:27:23 +0300

    modified

--- lib/keja_digital_web/live/user_reset_password_live.ex
+++ lib/keja_digital_web/live/user_reset_password_live.ex
@@ -3,42 +3,13 @@ defmodule KejaDigitalWeb.UserResetPasswordLive do
 
   alias KejaDigital.Store
 
-  def render(assigns) do
-    ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">Reset Password</.header>
+  alias KejaDigital.Store.User
 
-      <.simple_form
-        for={@form}
-        id="reset_password_form"
-        phx-submit="reset_password"
-        phx-change="validate"
-      >
-        <.error :if={@form.errors != []}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
-
-        <.input field={@form[:password]} type="password" label="New password" required />
-        <.input
-          field={@form[:password_confirmation]}
-          type="password"
-          label="Confirm new password"
-          required
-        />
-        <:actions>
-          <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
-        </:actions>
-      </.simple_form>
-
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
-      </p>
-    </div>
-    """
-  end
-
+  # Add lifecycle hook to prevent memory leaks
+  @impl true
   def mount(params, _session, socket) do
+    if connected?(socket), do: :timer.send_interval(1000 * 60 * 60, self(), :check_token_expiry)
+
     socket = assign_user_and_token(socket, params)
 
     form_source =
@@ -53,8 +24,63 @@ defmodule KejaDigitalWeb.UserResetPasswordLive do
     {:ok, assign_form(socket, form_source), temporary_assigns: [form: nil]}
   end
 
-  # Do not log in the user after reset password to avoid a
-  # leaked token giving the user access to the account.
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="mx-auto max-w-sm">
+      <.header class="text-center">Reset Password</.header>
+
+      <.simple_form
+        for={@form}
+        id="reset_password_form"
+        phx-submit="reset_password"
+        phx-change="validate"
+        phx-trigger-action={@trigger_submit}
+      >
+        <.error :if={@form.errors != []}>
+          Oops, something went wrong! Please check the errors below.
+        </.error>
+
+        <.input
+          field={@form[:password]}
+          type="password"
+          label="New password"
+          required
+          phx-debounce="blur"
+        />
+        <.input
+          field={@form[:password_confirmation]}
+          type="password"
+          label="Confirm new password"
+          required
+          phx-debounce="blur"
+        />
+
+        <:actions>
+          <.button
+            phx-disable-with="Resetting..."
+            class="w-full"
+            disabled={!@form.source.valid?}
+          >
+            Reset Password
+          </.button>
+        </:actions>
+      </.simple_form>
+
+      <p class="text-center text-sm mt-4">
+        <.link href={~p"/users/register"} class="font-semibold text-brand hover:underline">
+          Register
+        </.link>
+        |
+        <.link href={~p"/users/log_in"} class="font-semibold text-brand hover:underline">
+          Log in
+        </.link>
+      </p>
+    </div>
+    """
+  end
+
+  @impl true
   def handle_event("reset_password", %{"user" => user_params}, socket) do
     case Store.reset_user_password(socket.assigns.user, user_params) do
       {:ok, _} ->
@@ -68,22 +94,52 @@ defmodule KejaDigitalWeb.UserResetPasswordLive do
     end
   end
 
+  @impl true
   def handle_event("validate", %{"user" => user_params}, socket) do
     changeset = Store.change_user_password(socket.assigns.user, user_params)
     {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
   end
 
-  defp assign_user_and_token(socket, %{"token" => token}) do
-    if user = Store.get_user_by_reset_password_token(token) do
-      assign(socket, user: user, token: token)
+  @impl true
+  def handle_info(:check_token_expiry, socket) do
+    if token_expired?(socket.assigns.token) do
+      {:noreply,
+       socket
+       |> put_flash(:error, "Reset password link has expired. Please request a new one.")
+       |> redirect(to: ~p"/users/reset_password")}
     else
-      socket
-      |> put_flash(:error, "Reset password link is invalid or it has expired.")
-      |> redirect(to: ~p"/")
+      {:noreply, socket}
     end
+  end
+
+  defp assign_user_and_token(socket, %{"token" => token}) do
+    case Store.get_user_by_reset_password_token(token) do
+      %User{} = user ->
+        socket
+        |> assign(:user, user)
+        |> assign(:token, token)
+        |> assign(:trigger_submit, false)
+      nil ->
+        socket
+        |> put_flash(:error, "Reset password link is invalid or it has expired.")
+        |> redirect(to: ~p"/")
+    end
+  end
+
+  defp assign_user_and_token(socket, _params) do
+    socket
+    |> put_flash(:error, "Reset token is required")
+    |> redirect(to: ~p"/")
   end
 
   defp assign_form(socket, %{} = source) do
     assign(socket, :form, to_form(source, as: "user"))
+  end
+
+  defp token_expired?(token) do
+    case Store.get_user_by_reset_password_token(token) do
+      nil -> true
+      _user -> false
+    end
   end
 end
